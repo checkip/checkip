e46ab7291824144f33212537c46929d8f8b4b6c4
Dmitriy Tarasov <info@rubysamurai.com>
2021-01-26 23:42:01 +0300

    feat(ux): display unmodified geolocation data

--- frontend/components/checker/checker.js
+++
@@ -1,6 +0,0 @@
-import { MDCDataTable } from '@material/data-table';
-
-const tableEls = Array.from(document.querySelectorAll('.mdc-data-table'));
-tableEls.forEach((el) => {
-  MDCDataTable.attachTo(el);
-});
