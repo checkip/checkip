66e2e5acf4633b176a86539055f45165f441dc99
Dmitriy Tarasov <info@rubysamurai.com>
2021-07-26 15:31:30 +0300

    refactor(core): use guard clause for cli check

--- app/controllers/checkers_controller.rb
+++ app/controllers/checkers_controller.rb
@@ -2,13 +2,12 @@ class CheckersController < ApplicationController
   include CheckerScoped, CliDetection
 
   def new
-    if @cli_detected
-      respond_to do |format|
-        format.html do
-          render layout: false, plain: "#{@checker.ip}\n"
-        end
+    return unless @cli_detected
+
+    respond_to do |format|
+      format.html do
+        render layout: false, plain: "#{@checker.ip}\n"
       end
     end
-    @ua = request.user_agent
   end
 end
