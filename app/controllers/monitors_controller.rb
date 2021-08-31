4d0bfddd5e9842526a0698eec5bfd460ed2494d4
Dmitriy Tarasov <info@rubysamurai.com>
2021-08-31 15:46:44 +0300

    feat(core): style json on health check

--- app/controllers/monitors_controller.rb
+++ app/controllers/monitors_controller.rb
@@ -2,15 +2,17 @@ class MonitorsController < ApplicationController
   def show
     respond_to do |format|
       format.json do
-        render json: {
-          status: 'OK',
-          geo_database: {
-            connected: defined?(Checker::GEO_DB) ? true : false
-          },
-          asn_database: {
-            connected: defined?(Checker::ASN_DB) ? true : false
+        render json: JSON.pretty_generate(
+          {
+            status: 'OK',
+            geo_database: {
+              connected: defined?(Checker::GEO_DB) ? true : false
+            },
+            asn_database: {
+              connected: defined?(Checker::ASN_DB) ? true : false
+            }
           }
-        }
+        )
       end
     end
   end
