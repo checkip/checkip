2a02647080a861e033cf163ea916db257405a869
Dmitriy Tarasov <info@rubysamurai.com>
2021-08-31 16:29:58 +0300

    test: fix health check spec

--- spec/requests/monitors/show_spec.rb
+++ spec/requests/monitors/show_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 RSpec.describe 'monitors GET #show', type: :request do
   it 'responds with json' do
     get health_path
-    expect(response.body).to eq({
-      status: 'OK',
-      geo_database: {
-        connected: true
-      },
-      asn_database: {
-        connected: true
+    expect(JSON.parse(response.body, symbolize_names: true)).to eq(
+      {
+        status: 'OK',
+        geo_database: {
+          connected: true
+        },
+        asn_database: {
+          connected: true
+        }
       }
-    }.to_json)
+    )
   end
 end
