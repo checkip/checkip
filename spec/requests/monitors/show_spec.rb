559cd9b632e6a4266fa73b6085e21c2a5eab9ef3
Dmitriy Tarasov <info@rubysamurai.com>
2021-09-07 15:02:28 +0300

    test: request spec for non existing mmmdb files

--- spec/requests/monitors/show_spec.rb
+++ spec/requests/monitors/show_spec.rb
@@ -1,20 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe 'monitors GET #show', type: :request do
-  it 'responds with json' do
-    get health_path
-    expect(JSON.parse(response.body, symbolize_names: true)).to eq(
-      {
-        status: 'OK',
-        geo_database: {
-          connected: true,
-          version: '2020-12-29'
-        },
-        asn_database: {
-          connected: true,
-          version: '2020-12-29'
+  context 'when connected mmdb files' do
+    it 'responds with json' do
+      get health_path
+      expect(JSON.parse(response.body, symbolize_names: true)).to eq(
+        {
+          status: 'OK',
+          geo_database: {
+            connected: true,
+            version: '2020-12-29'
+          },
+          asn_database: {
+            connected: true,
+            version: '2020-12-29'
+          }
         }
-      }
-    )
+      )
+    end
+  end
+
+  context 'when not connected mmdb files' do
+    before do
+      Checker.send(:remove_const, 'GEO_DB') if defined?(Checker::GEO_DB)
+      Checker.send(:remove_const, 'ASN_DB') if defined?(Checker::ASN_DB)
+    end
+
+    it 'responds with json' do
+      get health_path
+      expect(JSON.parse(response.body, symbolize_names: true)).to eq(
+        {
+          status: 'OK',
+          geo_database: {
+            connected: false
+          },
+          asn_database: {
+            connected: false
+          }
+        }
+      )
+    end
   end
 end
