0bcb8829037acd2d2121edfbe3e6638100cea3a8
Dmitriy Tarasov <info@rubysamurai.com>
2021-10-21 00:14:28 +0300

    feat(ux): new navigation with drawer

--- frontend/components/layout-default/layout-default.js
+++ frontend/components/layout-default/layout-default.js
@@ -1,10 +1,31 @@
 import { MDCTopAppBar } from '@material/top-app-bar';
+import { MDCDrawer } from '@material/drawer';
 import { MDCRipple } from '@material/ripple';
-import { MDCTooltip } from '@material/tooltip';
 
 if (document.querySelector('.mdc-top-app-bar')) {
   const topAppBarElement = document.querySelector('.mdc-top-app-bar');
   MDCTopAppBar.attachTo(topAppBarElement);
+
+  if (document.querySelector('.mdc-drawer')) {
+    const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+    const topBar = MDCTopAppBar.attachTo(document.querySelector('.mdc-top-app-bar'));
+    const drawerListEl = document.querySelector('.mdc-drawer .mdc-deprecated-list');
+    const mainContentEl = document.querySelector('.ckp-layout-fixed');
+
+    topBar.listen('MDCTopAppBar:nav', () => {
+      drawer.open = true;
+    });
+
+    drawerListEl.addEventListener('click', () => {
+      drawer.open = false;
+    });
+
+    document.body.addEventListener('MDCDrawer:closed', () => {
+      if (mainContentEl.querySelector('input, button')) {
+        mainContentEl.querySelector('input, button').focus();
+      }
+    });
+  }
 }
 
 if (document.querySelector('.mdc-icon-button')) {
@@ -28,8 +49,3 @@ if (document.querySelector('#github-link')) {
     window.location.assign('https://github.com/checkip/checkip');
   });
 }
-
-const tooltipEls = Array.from(document.querySelectorAll('.mdc-tooltip'));
-tooltipEls.forEach((el) => {
-  MDCTooltip.attachTo(el);
-});
--- frontend/init/javascript/font-awesome.js
+++ frontend/init/javascript/font-awesome.js
@@ -2,10 +2,9 @@ import { config, library, dom } from '@fortawesome/fontawesome-svg-core';
 
 config.keepOriginalSource = false;
 
-import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faCode, faHeart, faHome, faUserShield } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faCode, faHeart, faGithub);
+library.add(faBars, faCode, faHome, faHeart, faGithub, faUserShield);
 
 dom.watch();
