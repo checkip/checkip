13c9dde45b1756eaf940be8b940401b2c0752bea
Dmitriy Tarasov <info@rubysamurai.com>
2021-10-30 00:40:14 +0300

    feat(ux): copy to clipboard button for the ip data

---
+++ frontend/components/checker/checker.js
@@ -0,0 +1,10 @@
+import { MDCRipple } from '@material/ripple';
+
+if (document.querySelector('.ckp-button-clipboard')) {
+  const demoCopy = document.querySelector('.ckp-button-clipboard');
+  MDCRipple.attachTo(demoCopy);
+
+  demoCopy.addEventListener('click', () => {
+    navigator.clipboard.writeText(demoCopy.dataset.clipboard);
+  });
+}
