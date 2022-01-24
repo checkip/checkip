188057f48d47061387e64bf5f20b69b74b25e4d1
Dmitriy Tarasov <info@rubysamurai.com>
2022-01-25 00:09:52 +0300

    build: replace webpacker with jsbundling-rails

--- config/webpack/production.js
+++
@@ -1,8 +0,0 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
-
-const environment = require('./environment')
-
-// Disable source maps in production
-environment.config.merge({ devtool: 'none' })
-
-module.exports = environment.toWebpackConfig()
