2db53406054fd342fb7c1c0cdf22b6ff2f28a17a
Dmitriy Tarasov <info@rubysamurai.com>
2022-09-15 10:48:43 +0300

    feat(ux): status page link (#20)

--- frontend/init/javascript/font-awesome.js
+++ frontend/init/javascript/font-awesome.js
@@ -2,9 +2,17 @@ import { config, library, dom } from '@fortawesome/fontawesome-svg-core';
 
 config.keepOriginalSource = false;
 
-import { faBars, faCode, faHeart, faHome, faUserShield } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBars,
+  faCode,
+  faHeart,
+  faHome,
+  faSignal,
+  faUserShield,
+} from '@fortawesome/free-solid-svg-icons';
+
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faBars, faCode, faHome, faHeart, faGithub, faUserShield);
+library.add(faBars, faCode, faHome, faHeart, faGithub, faSignal, faUserShield);
 
 dom.watch();
