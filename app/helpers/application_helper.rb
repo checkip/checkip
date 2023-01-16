d8e9c84c15f56552dac312632d6d53e52fbaf2fc
Dmitriy Tarasov <info@rubysamurai.com>
2023-01-16 17:10:26 +0300

    feat(ux): app version in footer (#25)
    Show app version based on `CHECKIP_SHOW_VERSION` value.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,18 @@ module ApplicationHelper
     lexer = Rouge::Lexer.find(language)
     formatter.format(lexer.lex(text))
   end
+
+  def app_name_footer
+    link_to full_app_name, 'https://github.com/checkip/checkip'
+  end
+
+  private
+
+    def full_app_name
+      if ENV['CHECKIP_SHOW_VERSION'] == 'true'
+        t('pages.application.core.app') + " v#{Checkip::Application::VERSION}"
+      else
+        t('pages.application.core.app')
+      end
+    end
 end
