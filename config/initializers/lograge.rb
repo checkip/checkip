267d76537ecd9b286f7e74bff1658131cbaa8e97
Dmitriy Tarasov <info@rubysamurai.com>
2023-10-26 16:37:24 +0300

    chore(infrastructure): update lograge config

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,7 +1,18 @@
 Rails.application.configure do
   config.lograge.enabled = true
+  config.lograge.keep_original_rails_log = true
+
+  config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
 
   config.lograge.formatter = Lograge::Formatters::Json.new
 
   config.lograge.ignore_actions = ['MonitorsController#show']
+
+  config.lograge.custom_options = lambda do |event|
+    { time: Time.now }
+  end
+
+  config.lograge.custom_payload do |controller|
+    { request_id: controller.request.request_id }
+  end
 end
